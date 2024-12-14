a41b0ac9a9e64482d835b48be4ec67c7cf819b78
Ckala62rus <agr.akyla@mail.ru>
2024-12-14 20:38:23 +0300

    ( feature ) update vue2 to vue3.

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -1,27 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import HomeView from '@/views/HomeView'
 
-Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/warehouse',
+        name: 'warehouse',
+        component: () => import('@/views/Warehouse.vue')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    }
 ]
 
-const router = new VueRouter({
-  routes
+const router = createRouter({
+    routes,
+    history: createWebHistory()
 })
 
 export default router
---
+++ src/src/views/Warehouse.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Warehouse
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "TemWarehouse"
+}
+</script>
+
+<style scoped>
+
+</style>
