4a3c392059016f612f99b82ab60361097c98fc94
Ckala62rus <agr.akyla@mail.ru>
2024-12-20 12:10:14 +0300

    ( feature ) added template components.

---
+++ src/src/views/template/Content.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+      <!--begin::Page-->
+      <div class="page d-flex flex-row flex-column-fluid">
+        <!--begin::Aside-->
+        <tmp-aside/>
+        <!--end::Aside-->
+        <!--begin::Wrapper-->
+        <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+          <!--begin::Header-->
+          <tmp-header/>
+          <!--end::Header-->
+          <!--begin::Content-->
+          <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+            <!--begin::Toolbar-->
+            <tmp-toolbar/>
+            <!--end::Toolbar-->
+            <!--begin::Post-->
+            <div class="post d-flex flex-column-fluid" id="kt_post">
+              <div id="kt_content_container" class="container-fluid">
+                <router-view></router-view>
+              </div>
+            </div>
+            <!--end::Post-->
+          </div>
+          <!--end::Content-->
+          <!--begin::Footer-->
+          <tmp-footer/>
+          <!--end::Footer-->
+        </div>
+        <!--end::Wrapper-->
+      </div>
+      <!--end::Page-->
+    </div>
+    <!--end::Root-->
+
+    <!--begin::Drawers-->
+    <!--begin::Activities drawer-->
+    <activities-drawer/>
+    <!--end::Activities drawer-->
+
+    <!--begin::Explorer drawer toggle-->
+    <explorer-drawer-toggle/>
+    <!--end::Explorer drawer toggle-->
+
+    <!--begin::Explorer drawer-->
+    <explorer-drawer/>
+    <!--end::Explorer drawer-->
+
+    <!--begin::Chat drawer-->
+    <chat-drawer/>
+    <!--end::Chat drawer-->
+    <!--end::Drawers-->
+
+    <!--begin::Modals-->
+    <!--begin::Modal - Invite Friends-->
+    <modal-invite-friends/>
+    <!--end::Modal - Invite Friend-->
+
+    <!--begin::Modal - Create App-->
+    <modal-create-app/>
+    <!--end::Modal - Create App-->
+
+    <!--begin::Modal - Upgrade plan-->
+    <modal-upgrade-plan/>
+    <!--end::Modal - Upgrade plan-->
+    <!--end::Modals-->
+
+    <!--begin::Scrolltop-->
+    <scroll-top/>
+    <!--end::Scrolltop-->
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TmpContent"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/src/views/template/ExplorerDrawerToggle.vue
@@ -0,0 +1,17 @@
+<template>
+  <button id="kt_explore_toggle"
+          class="btn btn-sm btn-white btn-active-primary shadow-sm position-fixed px-5 fw-bolder zindex-2 top-50 mt-10 end-0 transform-90"
+          title="Explore Metronic" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-trigger="hover">
+    <span id="kt_explore_toggle_label">Explore</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ExplorerDrawerToggle"
+}
+</script>
+
+<style scoped>
+
+</style>
