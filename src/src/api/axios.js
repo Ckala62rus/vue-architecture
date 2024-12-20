c6befc5c0ed9160f3589864e49f9bbf583b08647
Ckala62rus <agr.akyla@mail.ru>
2024-12-20 12:20:40 +0300

    ( feature ) added vue store.

--- src/src/api/axios.js
+++ src/src/api/axios.js
@@ -1,5 +1,27 @@
 import axios from "axios";
+import {getItem} from "@/helpers/persistancecStorage";
+// import router from "@/router";
 
-axios.defaults.baseURL = 'http://localhost:8001/api/v1'
+const env = process.env
+
+axios.defaults.baseURL = env.VUE_APP_BACKEND_API
+
+axios.interceptors.request.use( config => {
+    const token = getItem('access_token')
+    config.headers.Authorization = token ? `Bearer ${token}` : ''
+    return config
+})
+
+// axios.interceptors.response.use(undefined, (error) => {
+//     if (error.response.status === 401 && window.location.pathname !== '/login') {
+//         //place your reentry code
+//         localStorage.removeItem('access_token')
+//         // store.dispatch('user', null)
+//         delete axios.defaults.headers.common["Authorization"];
+//         console.log('interceptor')
+//         // window.location.href = '/login';
+//     }
+//     return Promise.reject(error);
+// });
 
 export default axios
