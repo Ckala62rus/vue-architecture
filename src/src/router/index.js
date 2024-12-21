1384a9d3086988105f381d66a60ce886a07923fc
Ckala62rus <agr.akyla@mail.ru>
2024-12-21 19:05:06 +0300

    ( feature ) added vue router with routs.

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -1,11 +1,15 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import MainPage from '@/views/template/MainPage'
+// import MainPage from '@/views/template/MainPage'
+import HomePage from "@/views/Pages/HomePage"
+import TmpForm from "@/views/Pages/Form"
 
 
 const routes = [
-    {path: '/', name: 'home', component: MainPage},
-    {path: '/warehouse', name: 'warehouse', component: () => import('@/views/Warehouse.vue')},
-    {path: '/login', name: 'login', component: () => import('@/views/LoginPage.vue')},
+    {path: '/', name: 'home', component: HomePage},
+    {path: '/form', name: 'form', component: TmpForm},
+    // // {path: '/', name: 'home', component: MainPage},
+    {path: '/warehouse', name: 'warehouse',  component: () => import('@/views/Warehouse.vue')},
+    {path: '/login', name: 'login', meta: { transition: 'slide-left' }, component: () => import('@/views/LoginPage.vue')},
     // {
     //     path: '/about',
     //     name: 'about',
