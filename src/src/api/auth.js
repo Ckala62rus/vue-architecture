582ffd6eea26ba4cb0c6cd1d581b56ff5934ca67
Ckala62rus <agr.akyla@mail.ru>
2024-12-21 18:55:16 +0300

    ( feature ) added auth axios logic.

---
+++ src/src/api/auth.js
@@ -0,0 +1,35 @@
+import axios from '@/api/axios'
+import urls from "@/api/urls";
+import {getItem} from "@/helpers/persistancecStorage";
+
+/**
+ * {
+ *   "email": "admin@mail.ru",
+ *   "password": "123123",
+ *   "username": "admin"
+ * }
+ *
+ * @param credential
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+const registration = credential => {
+    return axios.post(urls.registration, credential)
+}
+
+const login = credential => {
+    return axios.post(urls.login, credential)
+}
+
+const me = () => {
+    return axios(urls.me, {
+        headers: {
+            'Authorization': getItem('access_token')
+        }
+    })
+}
+
+export default {
+    registration,
+    login,
+    me
+}
