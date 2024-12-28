9d4539236fdedcac131c882779d55b8f1e94d6c0
Ckala62rus <agr.akyla@mail.ru>
2024-12-28 10:16:46 +0300

    ( feature ) vue router add rows.

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -1,15 +1,16 @@
 import {createRouter, createWebHistory} from 'vue-router'
-// import MainPage from '@/views/template/MainPage'
 import HomePage from "@/views/Pages/HomePage"
 import TmpForm from "@/views/Pages/Form"
+import TmpPasswordReset from "@/views/template/PasswordReset"
 
 
 const routes = [
     {path: '/', name: 'home', component: HomePage},
     {path: '/form', name: 'form', component: TmpForm},
-    // // {path: '/', name: 'home', component: MainPage},
+    {path: '/password-reset', name: 'password-reset', component: TmpPasswordReset},
     {path: '/warehouse', name: 'warehouse',  component: () => import('@/views/Warehouse.vue')},
     {path: '/login', name: 'login', meta: { transition: 'slide-left' }, component: () => import('@/views/LoginPage.vue')},
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: () => import('@/views/template/Page404') },
     // {
     //     path: '/about',
     //     name: 'about',
